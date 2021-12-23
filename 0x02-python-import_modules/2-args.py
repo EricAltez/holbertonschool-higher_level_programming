12900e4971cd9d031dcf6cf7d961906d46a95e25
EricAltez <3307@holbertonschool.com>
2021-12-23 18:54:44 +0000

    print elements in argv ex2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 0:
+        print("0 arguments.")
+    if len(argv) == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    if len(argv) > 2:
+        print("{:d} arguments:".format(len(argv)-1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
