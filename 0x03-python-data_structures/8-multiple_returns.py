20db13189c6222bf1bb355a0d6f6683f3e68eded
EricAltez <3307@holbertonschool.com>
2022-01-04 14:42:17 +0000

    corrected file 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     sentl = len(sentence)
     if sentl > 0:
-        return (sentl, sentence[0])
-    elif sentl == 0:
-        return (lsen, None)
+        return (tuple_a = (sentl, sentence[0]))
+    else:
+        return (tuple_a = (lsen, None))
