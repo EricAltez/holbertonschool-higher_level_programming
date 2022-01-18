d9bfd1921a437e1460e7f1c9a7ddb12d727781f1
EricAltez <ericaltez1111@gmail.com>
2022-01-18 10:58:38 -0800

    add file 1 comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""
+look! a coment!
+"""
 
 
 class Square:
+    """
+    is this the comment of Ester Piscore?
+    """
 
     def __init__(self, size=None):
+        """
+        No, this is the comment of Ester Piscore
+        """
         self.__size = size
