3f87d8b544d76d68082770ba2f6dbee9d12f8495
EricAltez <ericaltez1111@gmail.com>
2022-01-24 14:03:03 -0800

    vbt

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """define rectangle class elements"""
 
-        self.__width = width
-        self.__height = height
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
