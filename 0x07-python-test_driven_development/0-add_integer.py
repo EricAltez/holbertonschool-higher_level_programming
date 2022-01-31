a0ecc3ec66581e38c33b7e0da1efa46bd876621a
EricAltez <ericaltez1111@gmail.com>
2022-01-31 14:52:35 -0800

    add test file for file 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    if a is not int or a is not float:
+        raise TypeError("a must be an integer")
+    if b is not int or a is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
