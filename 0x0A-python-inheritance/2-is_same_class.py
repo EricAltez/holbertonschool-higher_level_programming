88002950f266ea180964ce5370f824d4bbbe9561
EricAltez <ericaltez1111@gmail.com>
2022-02-02 10:35:06 -0800

    add file 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Exact same object
+"""
+
+
+def is_same_class(obj, a_class):
+    """ return true or false """
+    if isinstance(obj, a_class) == True:
+        return True
+    else:
+        return False
