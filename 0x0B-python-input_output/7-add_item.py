f68fcf5c7007529ebccdb80c354c083ae65cfc59
EricAltez <ericaltez1111@gmail.com>
2022-02-01 16:03:41 -0800

    corrected file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    old_add = load_from_json_file('add_item.json')
+    lista = load_from_json_file('add_item.json')
     save_to_json_file(lista + sys.argv[1:], 'add_item.json')
 except Exception:
     save_to_json_file(sys.argv[1:], 'add_item.json')
