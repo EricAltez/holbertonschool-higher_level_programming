35159d131aac01764b359d75f25f35bddfd3d16b
EricAltez <ericaltez1111@gmail.com>
2022-02-01 17:27:58 -0800

    add file 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+student class module
+"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """ object initialization """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        aux = {}
+        if type(attrs) == list:
+            for key in attrs:
+                if type(key) != str:
+                    return self.__dict__
+                if key in self.__dict__:
+                    aux[key] = self.__dict__[key]
+            return aux
+        else:
+            return self.__dict__
