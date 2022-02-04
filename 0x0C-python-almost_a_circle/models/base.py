8d0766cfb0af5060b9c35a61b299d38ff94cb75a
EricAltez <ericaltez1111@gmail.com>
2022-02-04 04:51:50 -0800

    completed file 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,3 +7,10 @@
 class Base:
 
     def __init__(self, id=None):
+
+        __nb_objects = 0
+        if id != None:
+            id = None
+        else:
+            __nb_objects += 1
+            id = __nb_objects
