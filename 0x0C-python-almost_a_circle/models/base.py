4194c182dc5777b061737f171bd4754a284bf201
EricAltez <ericaltez1111@gmail.com>
2022-02-07 15:01:02 -0800

    add comments to base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-
+Base module
 """
 
 
 class Base:
-
+    """ Base class """
     __nb_objects = 0
 
     def __init__(self, id=None):
 
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
