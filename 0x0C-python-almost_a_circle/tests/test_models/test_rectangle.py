8e5670bb9205b7d4c2af0beb44a32a9c618b236a
EricAltez <ericaltez1111@gmail.com>
2022-02-08 12:39:48 -0800

    add doc test for rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """ Rectangle test module """
+
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class RectangleTests(unittest.TestCase):
     """ Rectangle test class"""
+
+    def test_documentation(self):
+        """documentation test"""
+        self.assertTrue(len(Rectangle.__doc__) > 0)
+        self.assertTrue(len(Rectangle.area.__doc__) > 0)
+        self.assertTrue(len(Rectangle.display.__doc__) > 0)
+        self.assertTrue(len(Rectangle.__str__.__doc__) > 0)
+        self.assertTrue(len(Rectangle.update.__doc__) > 0)
+        self.assertTrue(len(Rectangle.to_dictionary.__doc__) > 0) 
