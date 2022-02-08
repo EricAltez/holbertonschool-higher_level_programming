a2538561a3294f5e7b270f6e5ebcd1ae8b2f7cae
EricAltez <ericaltez1111@gmail.com>
2022-02-07 16:05:14 -0800

    add JSON str representation of list_objs to file to base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,15 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes JSON str repr of list_objs to file """
+        aux_l = []
+        with open('{}.json'.format(cls.__name__), 'w+') as my_file:
+            if list_objs is None:
+                my_file.write(cls.to_json_string(aux_l))
+            else:
+                for item in list_objs:
+                    aux_l.append(cls.to_dictionary(item))
+                my_file.write(cls.to_json_string(aux_l))
