2d4641b46598c93a721e1c9efd698453763f3c54
EricAltez <ericaltez1111@gmail.com>
2022-02-08 10:14:46 -0800

    add json test case

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ BaseTest module """
 
-
+from models.rectangle import Rectangle
 from models.base import Base
 import unittest
 
@@ -30,6 +30,15 @@ class BaseTest(unittest.TestCase):
         self.assertEquals(h.id, 89)
         h = Base(98)
         self.assertEquals(h.id, 98)
+
+    def test_to_json_string(self):
+        """ test json """
+        r1 = Rectangle(10, 7, 2, 8)
+        r1.id = 1
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertTrue(type(json_dictionary) is str)
+
 
 if __name__ == '__main__':
     unittest.main()
