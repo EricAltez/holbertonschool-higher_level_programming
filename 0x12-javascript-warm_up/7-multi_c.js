242fba7da969e1a9f028028d199f414558822d88
EricAltez <ericaltez1111@gmail.com>
2022-04-04 19:19:26 -0700

    corrected file 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   let i = 0;
-  while (i !== process.argv[2] && process.argv[2] > 0) {
+  while (i < process.argv[2] && process.argv[2] > 0) {
     console.log('C is fun');
     i++;
   }
