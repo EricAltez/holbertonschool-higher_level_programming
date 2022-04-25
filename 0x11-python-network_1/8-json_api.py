604e02cbe072711916600290c3b0d60cbf3f49f1
EricAltez <ericaltez1111@gmail.com>
2022-04-25 07:02:13 -0700

    add file 8 for test

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if (len(argv) > 1):
+        sq = {"q": argv[1]}
+    else:
+        sq = {"q": ""}
+
+    x = request.post(http://0.0.0.0:5000/search_user, data=sq)
+    try:
+        res = x.json()
+        if len(res) == 0:
+            print ("No result")
+        else::
+            print ("[{}] {}".format(res.get("id"), res.get("name")))
+    except Exception:
+    print("Not a valid JSON")
