24cc20afd0d09c0dd6c9eac89da058068c61da0a
EricAltez <ericaltez1111@gmail.com>
2022-04-24 18:40:15 -0700

    add file 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    x = requests.get(argv[1])
+    if x.status_code >= 400:
+        print("Error code: {}".format(x.status_code))
+    else:
+        print(x.text)
