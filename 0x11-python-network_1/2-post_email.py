b367165282fe6f8e8d2ab5cd3e8d2e62b36d77c4
EricAltez <ericaltez1111@gmail.com>
2022-04-24 17:21:22 -0700

    modd file2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+Takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
 """
 import urllib.request
 import urllib.parse
@@ -12,6 +14,6 @@ values = {'email': email}
 data = urllib.parse.urlencode(values)
 data = data.encode('ascii')
 req = urllib.request.Request(argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-        print(the_page.decode('utf-8'))
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+    print(the_page.decode('utf-8'))
