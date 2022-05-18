c51b8afe8450f63d1a02766402d9ea1f81647258
EricAltez <ericaltez1111@gmail.com>
2022-05-17 18:55:57 -0700

    modd file 4, search id on string

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,20 @@
 /* Prints the title of a Star Wars movie for given character */
 
 const axios = require('axios').default;
-const characterx = 'https://swapi-api.hbtn.io/api/people/18/';
-if (process.argv.length >= 3) {
-  axios.get(process.argv[2])
-    .then(function (response) {
-      let num = 0;
-      for (const x in response.data.results) {
-        if (response.data.results[x].characters.includes(characterx)) {
+axios.get(process.argv[2])
+  .then(function (response) {
+    let num = 0;
+    for (const x in response.data.results) {
+      for (const y in response.data.results[x].characters) {
+        const id = response.data.results[x].characters[y].split('/')[5];
+        if (id === '18') {
           num = num + 1;
         }
       }
-      console.log(num);
-    })
-    .catch(function (error) {
-      error = '0';
-      console.log(error);
-    });
-}
+    }
+    console.log(num);
+  })
+  .catch(function (error) {
+    error = '0';
+    console.log(error);
+  });
