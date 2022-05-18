c8668562a4908f279929d1a56302faa5493aabc8
EricAltez <ericaltez1111@gmail.com>
2022-05-17 17:57:38 -0700

    add length check to file 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
 /* Prints the title of a Star Wars movie for given character */
+
 const axios = require('axios').default;
-const starWars = 'https://swapi-api.hbtn.io/api/people/18/';
-axios.get(process.argv[2])
-  .then(function (response) {
-    let num = 0;
-    for (const x in response.data.results) {
-      if (response.data.results[x].characters.includes(starWars)) {
-        num = num + 1;
+const characterx = 'https://swapi-api.hbtn.io/api/people/18/';
+if (process.argv.length >= 3) {
+  axios.get(process.argv[2])
+    .then(function (response) {
+      let num = 0;
+      for (const x in response.data.results) {
+        if (response.data.results[x].characters.includes(characterx)) {
+          num = num + 1;
+        }
       }
-    }
-    console.log(num);
-  })
-  .catch(function (error) {
-    error = '0';
-    console.log(error);
-  });
+      console.log(num);
+    })
+    .catch(function (error) {
+      error = '0';
+      console.log(error);
+    });
+}
